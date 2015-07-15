e02983511323079cf48822bf450287ef52770a6b
Anastasiya Zaslavskaya <zaslavskayasya@gmail.com>
2015-07-15 16:53:07 +0300

    opacity-2

--- app/index.php
+++ app/index.php
@@ -13,30 +13,15 @@ include "tmpl/header.php";
 
         <div class="workspace-inner clearfix">
           <div class="workspase__uploud-image">
-<<<<<<< .merge_file_a08976
-            <!--область для водяного знака, видима уже при его загрузке-->
-            <div class="workspase_wotermark-wrap">
-              <div class="workspase__wotermark workspase__wotermark-image"><img id="blah" src="#" alt="your image" /></div>
-              <div class="workspase__wotermark workspase__wotermark-watermark" id="watermark-opacity"><img id="blah2" src="#" alt="your watermark"/><span id="info">dragging opacity : 0.5</span></div>
 
-
-<!-- HTML <div>
-    <div id="dragItem" class="square"></div>                   
-</div> 
-
--->
-
-
-
-=======
             <!--область для загрузки каринки, видима при загрузке картинки-->
             <div class="workspase__wotermark workspase__wotermark-image">
               <img id="blah" src="#" alt="your image" />
               <!--область для водяного знака, видима уже при его загрузке-->
               <div class="workspase_wotermark-wrap">
-                <div class="workspase__wotermark workspase__wotermark-watermark"><img id="blah2" src="#" alt="your watermark" /></div>
+                <div class="workspase__wotermark workspase__wotermark-watermark" id="watermark-opacity"><img id="blah2" src="#" alt="your watermark" /></div>
               </div>
->>>>>>> .merge_file_a08616
+
             </div>
 
           </div>
