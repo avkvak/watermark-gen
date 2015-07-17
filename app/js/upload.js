c31d51f2f341f8e47f84b025df0324115eaad30e
DimaMinka <cdk@cdk.co.il>
2015-07-17 10:28:30 +0300

    set W and H to watemark wrapon load

--- app/js/upload.js
+++ app/js/upload.js
@@ -15,8 +15,24 @@ var imgUpload = (function () {
         dataType: 'json',
         done: function (e, data) {
             $.each(data.result.files, function (index, file) {
+
                 $('#watermark__src-logo').attr('src', 'uploads/files/' + file.name);
                 $('#input-watermark').attr('value', file.name);
+
+                function realImgDimension(img) {
+                    var i = new Image();
+                    i.src = img.src;
+                    return {
+                        naturalWidth: i.width,
+                        naturalHeight: i.height
+                    };
+                }
+                var myImage = document.getElementById('watermark__src-logo');
+                myImage.addEventListener('load', function() {
+                    var realSize = realImgDimension(this);
+                    $('.workspase_wotermark-wrap').width(realSize.naturalWidth).height(realSize.naturalHeight);
+                });
+
             });
         }
     });
