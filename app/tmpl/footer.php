313d88bc7c9803c7555f8ecc6fe96ce51449093c
Anastasiya Zaslavskaya <zaslavskayasya@gmail.com>
2015-07-17 21:02:49 +0300

    - footer conflict

--- app/tmpl/footer.php
+++ app/tmpl/footer.php
@@ -6,16 +6,11 @@
 
 <!-- build:js js/main.js -->
 <script src="js/main.js"></script>
-<<<<<<< HEAD
+
 <!-- endbuild -->
 <script src="js/opacity.js"></script>
 <script src="js/reset.js"></script>
-=======
 <script src="js/position.js"></script>
 
-<!-- endbuild -->
-<script src="bower/jquery-ui/jquery-ui.min.js"></script>
-<script src="js/opacity-bottom.js"></script>
->>>>>>> 4c6de9792edd2061a2b534e733911aa711f49662
 </body>
 </html>
