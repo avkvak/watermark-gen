2beb8e7f30743bf85de33b45dfe38c03111b7ca4
Anastasiya Zaslavskaya <zaslavskayasya@gmail.com>
2015-07-25 15:55:25 +0300

    reset. no var )

--- app/js/reset.js
+++ app/js/reset.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
 $('.controllers-form').on('reset', function resetFunction2(){
-    var resetWatermark = $('#watermark__src-image').attr({"src": "img/image.jpg"});
-    var resetPicture = $('#watermark__src-logo').attr({"src": "img/watermark.png"});
+    $('#watermark__src-image').attr({"src": "img/image.jpg"});
+    $('#watermark__src-logo').attr({"src": "img/watermark.png"});
+    $('.workspase__wotermark-watermark').css('opacity','1');
+  
    
 });
-}); 
+});
